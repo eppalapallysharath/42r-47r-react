836c0706deb2ea3692706a7e6c4ab47e47e87e66
eppalapallysharath <eppalapallysharath@gmail.com>
2025-10-21 14:07:15 +0530

    list rendering

--- compoents2/src/App.jsx
+++ compoents2/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "./components/header/Header"
 import Banner from "./components/sections/Banner"
 import Footer from "./components/footer/Footer"
-
+import Products from "./components/products";
+import ListCategory from "./components/CategoryList"
 class App extends React.Component{
     render(){
        return <>
-        <Header/>
+        {/* <Header/>
         <Banner/>
-        <Footer/>
+        <Footer/> */}
+        {/* <ListCategory/> */}
+        <Products/>
        </> 
     }
 }
